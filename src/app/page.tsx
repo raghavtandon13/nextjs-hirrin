acc5871e467e13542e99b1623e9be8c9020467c6
Raghav <raghav.tandon13@gmail.com>
2024-01-20 21:11:58 +0530

    yep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between pt-[70px] md:flex-row lg:mx-[10%]`}>
       <div className="flex flex-1 flex-col items-center justify-center">
-        <h1 className="font-clash font-black text-[40px] text-center text-gray-800 lg:text-[8vh]">
+        <h1 className="font-clash font-black text-[40px] text-center text-gray-800 px-5 lg:text-[8vh]">
           Your <span className="mx-1">Dream</span> Job
         </h1>
         <h1 className="font-clash font-black text-[40px] text-center text-gray-800 lg:text-[8vh]">is Waiting</h1>
