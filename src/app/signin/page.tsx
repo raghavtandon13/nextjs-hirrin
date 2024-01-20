e698a098ff3c57e7ee34168c450016d4fe3354ed
Raghav <raghav.tandon13@gmail.com>
2024-01-20 17:30:25 +0530

    footer

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className=" pt-[70px]">
+    <div className="border-2  border-gray-800 pt-[70px]">
       <h1>sign in page</h1>
     </div>
   );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-full h-max bg-gray-100 border-t border-gray-300 flex flex-col p-10 sm:flex-row">
+      <div className="flex flex-col flex-1 sm:flex-row">
+        <div className="flex flex-1 flex-col items-start justify-center">
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Search for Jobs</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Resume Tips</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Cover Letter Guide</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Interview Preparation</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Explore Companies</p>
+          </Link>
+        </div>
+        <div className="flex flex-1 flex-col items-start justify-center">
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Industry News</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Professional Networking</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Online Courses</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Career Blog</p>
+          </Link>
+          <Link href="#">
+            <p className="text-gray-700 hover:text-blue-500">Set Job Alerts</p>
+          </Link>
+        </div>
+      </div>
+      <hr className="w-full h-px my-5 bg-gray-300 sm:hidden" />
+      <div className="flex flex-1 flex-col items-start justify-center sm:items-end">
+        <Link href="#">
+          <p className="text-gray-700 hover:text-blue-500">Terms of Service</p>
+        </Link>
+        <Link href="#">
+          <p className="text-gray-700 hover:text-blue-500">Privacy Policy</p>
+        </Link>
+        <Link href="#">
+          <p className="text-gray-700 hover:text-blue-500">Contact Us</p>
+        </Link>
+        <Link href="#">
+          <p className="text-gray-700 hover:text-blue-500">About Us</p>
+        </Link>
+        <Link href="#">
+          <p className="text-gray-700 hover:text-blue-500">Feedback Form</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
